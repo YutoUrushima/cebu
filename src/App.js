2bf1f63b82c83e2fac3eeda0eca94ceabf17ed1b
YutoUrushima <yuto259233@gmail.com>
2022-07-16 19:45:19 +0900

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const endpointURL = 'https://api.twitter.com/2/users/by';
+const token = process.env.REACT_APP_BEARER_TOKEN;
+
+const App = async () => {
+	const [state, setState] = useState();
+	await axios(endpointURL, {
+		headers: {
+			'authorization': `Bearer ${token}`,
+			'Access-Control-Allow-Origin': '*',
+		},
+		params: {
+			usernames: 'developer_japan',
+		},
+	})
+		.then((res) => {
+			console.log(res);
+			setState(res);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+			<p>{state}</p>
+		</div>
+	);
+};
 
 export default App;
